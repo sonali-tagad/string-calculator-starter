8125ddd15108d91e213eaa77065403c4f78826e7
Sonali Tagad <tagad.sonali99@gmail.com>
2021-10-17 21:47:57 +0530

    implementing : Support different delimiters

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,14 +3,24 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        String[] arr = input.split(",|n");
-        int sum = 0;
-            for (String element : arr) {
-                if (!element.trim().isEmpty()) {
-                    sum = sum + Integer.parseInt(element);
-                }
-            }
-        return sum;
+        String delim = ",|n";
+        String num = input;
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("//") + 2;
+            delim = input.substring(delimiterIndex, delimiterIndex + 1);
+            num = input.substring(input.indexOf("n") + 1);
+        }
+        return add(num, delim);
     }
 
+    private static int add(String num, final String delim) {
+        int sum = 0;
+        String[] arr = num.split(delim);
+        for (String element : arr) {
+            if (!element.trim().isEmpty()) {
+                sum += Integer.parseInt(element.trim());
+            }
+        }
+        return sum;
+    }
 }
