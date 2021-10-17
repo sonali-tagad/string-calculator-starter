b988b38e6172364aa116a9523cda6389ec371c49
Sonali Tagad <tagad.sonali99@gmail.com>
2021-10-17 20:47:30 +0530

    seperate the string using comma and collect the result in arr

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,9 +3,16 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        if (input == null)
-        {
+        String[] arr = input.split(",");
+        if (input == null) {
             return 0;
+        }
+        else {
+            for (String element : arr) {
+                if (!element.isEmpty()) {
+                    Integer.parseInt(element);
+                }
+            }
         }
         return 1;
     }
