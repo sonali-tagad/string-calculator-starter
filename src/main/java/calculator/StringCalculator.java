543ec2e89a83f976df197b416c99b6f2f9f586b6
Sonali Tagad <tagad.sonali99@gmail.com>
2021-10-17 22:00:43 +0530

    implementing : adding exception “negatives not allowed”

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class StringCalculator {
 
     public int add(String input) {
@@ -10,16 +13,24 @@ class StringCalculator {
             delim = input.substring(delimiterIndex, delimiterIndex + 1);
             num = input.substring(input.indexOf("n") + 1);
         }
-        return add(num, delim);
+        return add(num,delim);
     }
 
     private static int add(String num, final String delim) {
         int sum = 0;
         String[] arr = num.split(delim);
+        List negativeNum = new ArrayList();
         for (String element : arr) {
             if (!element.trim().isEmpty()) {
-                sum += Integer.parseInt(element.trim());
+                int number = Integer.parseInt(element.trim());
+                if(number < 0){
+                    negativeNum.add(number);
+                }
+                sum += number;
             }
+        }
+        if (negativeNum.size() > 0) {
+            throw new RuntimeException("Negatives not allowed: " + negativeNum.toString());
         }
         return sum;
     }
