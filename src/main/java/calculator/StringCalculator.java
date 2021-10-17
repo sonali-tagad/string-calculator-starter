158cfd9cbd4d94f5f80ded18daeb739dbc12460f
Sonali Tagad <tagad.sonali99@gmail.com>
2021-10-17 20:23:07 +0530

    test case 1 : for empty string return 0

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,8 +3,11 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-
-        return 0;
+        if (input == null)
+        {
+            return 0;
+        }
+        return 1;
     }
 
 }
